5889b2ddd9f7e65a9c23d7a8546a3f79a52a12e1
Md Dilshan <158074772+Imdilshan@users.noreply.github.com>
2025-02-25 18:24:34 +0530

    ...

---
+++ Unit 1/ Employee Details .cpp
@@ -0,0 +1,41 @@
+// WAP to Create a class called employee
+#include <bits/stdc++.h>
+using namespace std;
+
+class employee{
+	string name, designation;
+	int age;
+	float salary;
+	public:
+	void get(){
+		cin >> name;
+		cin >> age;
+		cin.ignore();
+		getline(cin,designation);
+		cin >> salary;
+	}
+	void put(){
+		cout << "Name: " << name << endl;
+		cout << "Age: " << age << endl;
+		cout << "Designation: " <<  designation << endl;
+		cout << "Salary: " << fixed << setprecision(2) << salary <<  endl;
+	}
+};
+
+int main(){
+	int n;
+	cin >> n;
+	
+	employee e[n];
+	
+	for(int i = 0; i < n; i++){
+		e[i].get();
+	}
+
+	for(int i = 0; i < n; i++){
+		cout << "Employee " << i+1 << ":" << endl;
+		e[i].put();
+		cout << endl;
+	}
+	return  0;
+}
