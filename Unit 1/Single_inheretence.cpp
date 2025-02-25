d1ab1be9f2e732e45e26110232f498783449ee45
Md Dilshan <158074772+Imdilshan@users.noreply.github.com>
2025-02-25 18:32:42 +0530

    ...

---
+++ Unit 1/Single_inheretence.cpp
@@ -0,0 +1,20 @@
+// WAP to demonstrate the concept of Single inheritance.
+#include <bits/stdc++.h>
+using namespace std;
+
+class vehicle{
+public:
+	void display(){
+		cout << "This is a vehicle" << endl;
+	}
+};
+
+class car : public vehicle{
+	
+};
+
+int main(){
+	car obj;
+	obj.display();
+	return 0;
+}
