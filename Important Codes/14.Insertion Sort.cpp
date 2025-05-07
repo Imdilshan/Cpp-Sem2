f29a7ae79eb98b49b2006ac964146476a4a5e7f5
highfive <imdilshan@proton.me>
2025-05-08 01:24:54 +0530

    Update 14.Insertion Sort.cpp

--- Important Codes/14.Insertion Sort.cpp
+++ Important Codes/14.Insertion Sort.cpp
@@ -3,13 +3,15 @@ using namespace std;
 
 int main(){
 	int n;
+	cout << "Enter size of array: ";
 	cin >> n;
 	
 	int arr[n];
+	cout << "Enter elements of array: ";
 	for(int i = 0; i < n; i++){
 		cin >> arr[i];
 	}
-	
+	// Sorting using Insertion sort(Inserting elements in their correct position)
 	for(int i = 1; i < n; i++){
 		int j = i;
 		while(arr[j-1] > arr[j] && j > 0){
@@ -17,10 +19,9 @@ int main(){
 			j--;
 		}
 	}
-	
+	cout << "Elements after sorting: ";
 	for(int i = 0; i < n; i++){
 		cout << arr[i] << " ";
 	}
-	
 	return 0;
 }
