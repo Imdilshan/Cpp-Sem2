7fd37b0a0ecc045f41938332c58500fe5c947ad9
highfive <imdilshan@proton.me>
2025-05-08 01:19:35 +0530

    Update 12.Bubble Sort.cpp

--- Important Codes/12.Bubble Sort.cpp
+++ Important Codes/12.Bubble Sort.cpp
@@ -3,13 +3,16 @@ using namespace std;
 
 int main(){
 	int n;
+	cout << "Enter size of array: ";
 	cin >> n;
 	
 	int arr[n];
+	cout << "Enter elements of array: ";
 	for(int i = 0; i < n; i++){
 		cin >> arr[i];
 	}
 	
+	// Sorting using Bubble sort(Comparing 2 adjacent elements)
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < n-i-1; j++){
 			if(arr[j] > arr[j+1]){
@@ -17,10 +20,10 @@ int main(){
 			}
 		}
 	}
-	
+	cout << "Elements after sorting: ";
 	for(int i = 0; i < n; i++){
 		cout << arr[i] << " ";
 	}
-	
+	cout << endl;
 	return 0;
 }
