cf445447dab80f6977c4c2105fbc35a5190a0f6a
highfive <imdilshan@proton.me>
2025-05-08 01:14:18 +0530

    Update 09.Binary Search.cpp

--- Important Codes/09.Binary Search.cpp
+++ Important Codes/09.Binary Search.cpp
@@ -3,31 +3,34 @@ using namespace std;
 
 int main(){
 	int n;
+	cout << "Enter size of array: ";
 	cin >> n;
 	
 	int arr[n];
+	cout << "Enter array elements: ";
 	for(int i = 0; i < n; i++){
 		cin >> arr[i];
 	}
 	sort(arr, arr + n);
 	
-	int input;
-	cin >> input;
+	int num;
+	cout << "Enter number to search: ";
+	cin >> num;
 	
 	int l = 0, r = n-1, mid;
 	while(l <= r){
 		mid = (l + r) / 2; 
-		if(arr[mid] == input){
-			cout << mid << endl;
+		if(arr[mid] == num){
+			cout << arr[mid] << " found at position: " << mid << endl;
 			return 0;
 		}
-		else if(arr[mid] < input){
+		else if(arr[mid] < num){
 			l = mid + 1; 
 		}
 		else{
 			r = mid - 1;
 		}
 	}
-	cout << input << " not found." << endl;
+	cout << num << " not found." << endl;
 	return 0;
 }
