c58a47d860c60ac37bc7a2afbe806fd364665db2
highfive <imdilshan@proton.me>
2025-05-08 01:22:35 +0530

    Update 13.Selection Sort.cpp

--- Important Codes/13.Selection Sort.cpp
+++ Important Codes/13.Selection Sort.cpp
@@ -3,13 +3,15 @@ using namespace std;
 
 int main(){
 	int n;
+	cout << "Enter size of array: ";
 	cin >> n;
 	
 	int arr[n];
+	cout << "Enter elements of array: ";
 	for(int i = 0; i < n; i++){
 		cin >> arr[i];
 	}
-	
+	// Sorting using Selection sort(Swapping after selecting smallest value)
 	for(int i = 0; i < n-1; i++){
 		int min = i;
 		for(int j = i+1; j < n; j++){
@@ -19,7 +21,7 @@ int main(){
 		}
 		swap(arr[i],arr[min]);
 	}
-	
+	cout << "Elements after sorting: ";
 	for(int i = 0; i < n; i++){
 		cout << arr[i] << " ";
 	}
